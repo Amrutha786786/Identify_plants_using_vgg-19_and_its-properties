6b5172c5ca96c650e02531680aa238f67fdd5f29
Amrutha786786 <127930424+Amrutha786786@users.noreply.github.com>
2023-03-17 22:25:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,13 @@ def predict():
         x = x / 255
         x = np.expand_dims(x, axis=0)
         preds = model.predict(x)
-        l=[]
+        l=['Citrus Limon (Lemon)', 'Tabernaemontana Divaricata (Crape Jasmine)', 'Muntingia Calabura (Jamaica Cherry-Gasagase)', 'Alpinia Galanga (Rasna)', 
+           'Santalum Album (Sandalwood)', 'Jasminum (Jasmine)','Basella Alba (Basale)', 'Artocarpus Heterophyllus (Jackfruit)', 'Murraya Koenigii (Curry)',
+           'Ficus Religiosa (Peepal Tree)', 'Punica Granatum (Pomegranate)', 'Mangifera Indica (Mango)', 'Nerium Oleander (Oleander)', 'Carissa Carandas (Karanda)',
+           'Psidium Guajava (Guava)', 'Hibiscus Rosa-sinensis', 'Syzygium Cumini (Jamun)', 'Syzygium Jambos (Rose Apple)', 'Mentha (Mint)',
+           'Ficus Auriculata (Roxburgh fig)', 'Amaranthus Viridis (Arive-Dantu)', 'Ocimum Tenuiflorum (Tulsi)', 'Pongamia Pinnata (Indian Beech)',
+           'Nyctanthes Arbor-tristis (Parijata)', 'Brassica Juncea (Indian Mustard)', 'Trigonella Foenumgraecum (Fenugreek)', 'Piper Betle (Betel)', 
+           'Moringa Oleifera (Drumstick)', 'Azadirachta Indica (Neem)', 'Plectranthus Amboinicus (Mexican Mint)']
         pred_index = np.argmax(preds)
         result = l[pred_index].upper()
         return result
